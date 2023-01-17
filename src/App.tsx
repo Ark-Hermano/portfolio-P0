c6432f01977612c0b9b2a9210fdf068cb1df7eb4
Hermano Fernandes <hermano@avanz.com.br>
2023-01-16 21:07:42 -0300

    test: testing git changes

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ function App() {
   return (
     <div className="background">
       <div>
-
         <div className="head">
           <div className="content">
             <div className="nav">
