441a573838cc5c23b2c56ef2f1f1776948e04bbe
Ark-Hermano <trnhermann@gmail.com>
2023-02-03 23:45:05 -0300

    feat: removing comments

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,27 @@ function App() {
   const [certifications, setCertifications] = useState<any[]>([])
   const linguages = [
     {
-      name: 'Javascript', frameworks: [{
-        name: 'React',
-      },
-      {
-        name: 'React Native',
-      },
-      {
-        name: 'NextJS'
-      }]
+      name: 'Javascript',
+      frameworks: [
+        {
+          name: 'React',
+        },
+        {
+          name: 'React Native',
+        },
+        {
+          name: 'NextJS'
+        }
+      ]
     },
-    { name: 'PHP' },
+    {
+      name: 'PHP',
+      frameworks: [
+        {
+          name: 'Laravel',
+        },
+      ]
+    },
   ]
 
 
@@ -55,81 +65,7 @@ function App() {
 
   return (
     <main>
-      {/* <div>
-        <div className="head">
-          <div className="content">
-            <div className="nav">
-              <div className='site-title'>
-                <h1 className='title'>Título</h1>
-              </div>
 
-              <div className="sections-urls-container">
-                <ul className="section-urls">
-                  <li className="item">
-                    <div className="link">
-                      <a>
-                        link
-                      </a>
-                    </div>
-                  </li>
-                  <li className="item">
-                    <div className="link">
-                      <a>
-                        link
-                      </a>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className='highlighted'>
-          <div className="content">
-            <div className="main-label">Main Personal Projects and Jobs</div>
-            <ul className="project-list">
-              <li className="project">
-                <a>
-                  <div className="project">
-                    <img src="" />
-                  </div>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className='projects'>
-          <div className="nav">
-            <div className="swtich">
-              <div>trabalho</div>
-              <div>projetos</div>
-            </div>
-
-            <div className="listing ">
-              <ul className='list-type'>
-                <li>
-                  <div className="item">mobile</div>
-                </li>
-                <li>
-                  <div className="item">front</div>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className='carousel'>
-
-          </div>
-        </div>
-
-        <div className='jobs'>
-
-        </div>
-        <div className='certifications'></div>
-        <div className='educational-institutions'></div>
-      </div> */}
 
       <section className='head'>
         <div className='content'>
@@ -168,8 +104,6 @@ function App() {
                 </div>
               </div>
             ))}
-
-
           </div>
           <div className='repo-grid__options'>
             <button disabled={!!!repoGridPageIndex} onClick={() => handlePrev()} >
